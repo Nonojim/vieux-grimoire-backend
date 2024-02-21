8e0d308f2d417601c919ef97f8c02a55647bff81
Nonojim <nolan.pederencino@gmail.com>
2024-02-21 17:09:45 +0100

    thing to book

--- routes/stuff.js
+++ routes/stuff.js
@@ -5,10 +5,10 @@ const router = express.Router();
 
 const stuffCtrl = require('../controllers/stuff');
 
-router.post('/', auth, multer, stuffCtrl.createThing);
-router.put('/:id', auth, multer, stuffCtrl.modifyThing);
-router.delete('/:id', auth, stuffCtrl.deleteThing);
-router.get('/:id', auth, stuffCtrl.getOneThing);
-router.get('/', auth, stuffCtrl.getAllThings);
+router.post('/', auth, multer, stuffCtrl.createBook);
+router.put('/:id', auth, multer, stuffCtrl.modifyBook);
+router.delete('/:id', auth, stuffCtrl.deleteBook);
+router.get('/:id', auth, stuffCtrl.getOneBook);
+router.get('/', auth, stuffCtrl.getAllBooks);
 
 module.exports = router;
